6fcd5099f46c3529571d6eba41c0b3a0f5a23bd5
Neaz Mahmood <neazn694@gmail.com>
2024-08-21 02:44:10 +0600

    implement table

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -28,8 +28,45 @@ const Profile = () => {
   }, [])
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2 text-black font-medium text-xl'>
-      Profile
+    <div className='flex flex-col items-center justify-center min-h-screen w-screen py-2 text-black font-medium text-xl'>
+      <div className='w-full px-10'>
+        <div className='flex  px-5 text-lg font-semibold'>
+          <div className='w-[10%] text-start pl-1 border-solid border-2 border-black'>
+            id
+          </div>
+          <div className='w-[15%]  pl-1 border-solid border-2 border-black'>
+            Name
+          </div>
+          <div className='w-[30%]  pl-1 border-solid border-2 border-black'>
+            Phone
+          </div>
+          <div className='w-[30%]  pl-1 border-solid border-2 border-black'>
+            Email
+          </div>
+          <div className='w-[15%]  pl-1 border-solid border-2 border-black'>
+            City
+          </div>
+        </div>
+        {data?.map((item: any, index: number) => (
+          <div key={index} className='flex px-5 '>
+            <div className='w-[10%] text-start pl-1 border-solid border-2 border-black'>
+              {item?.id}
+            </div>
+            <div className='w-[15%] text-start pl-1 border-solid border-2 border-black'>
+              {item?.name}
+            </div>
+            <div className='w-[30%] text-start pl-1 border-solid border-2 border-black'>
+              {item?.phone}
+            </div>
+            <div className='w-[30%] text-start pl-1 border-solid border-2 border-black'>
+              {item?.email}
+            </div>
+            <div className='w-[15%] text-start pl-1 border-solid border-2 border-black'>
+              {item?.address?.city}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
